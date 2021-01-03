034588de29349d6106eebfb742a9aaacb634e591
Jhn <jhonata.tenorio2526@gmail.com>
2021-01-02 22:21:32 -0300

    end scraping

--- index.js
+++ index.js
@@ -3,39 +3,36 @@ const puppeteer = require('puppeteer');
 const start = async () => {
     const browser = await puppeteer.launch( { headless: true } );
     const page = await browser.newPage();
-    await page.goto('https://github.com/backend-br/vagas/issues');
+    await page.goto('https://github.com/backend-br/vagas/issues?q=is%3Aopen+is%3Aissue+label%3APHP');
 
-    const datas = await page.evaluate( () => {
-        const datas = new Array();
-
+    const datas = await page.evaluate( () => { // Init web-page script
+        let datas = new Array();
+        
         const container = document.querySelectorAll(".flex-auto.min-width-0.p-2.pr-3.pr-md-2");
         
         for(let i = 0; i < container.length; i++){
+            let requirements = new Array();
             const containerInfo = container[i].innerText;
             
-            datas[i] = {
-                name: containerInfo,
-                requirements: ["Requisitos: "]
-            }
-
+            
             let labels = document.querySelectorAll(".labels.lh-default.d-block.d-md-inline")[i].querySelectorAll(".IssueLabel.hx_IssueLabel");
             
-            if(labels.length <= i) break;
+            if(i >= labels.length) break;
             
             for(let j = 0; j < labels.length; j++){
                 console.log(labels.length);
-    
-                labels = labels[j].innerText;
-                
-    
-                datas[i].requirements.push(labels)
+                requirements.push(labels[j].innerText);
+            }
+
+            datas[i] = {
+                name: containerInfo,
+                requirements: requirements
             }
         }
         return datas;
     });
 
-
-    console.log(datas);
+    console.log(datas[0]);
 
     await browser.close();
 };
